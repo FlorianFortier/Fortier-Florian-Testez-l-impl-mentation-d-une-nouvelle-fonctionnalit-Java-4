5f9b08c12ec01da791d3b2adb3b80671525835dd
Florian Fortier <78166457+FlorianFortier@users.noreply.github.com>
2023-05-05 14:16:15 +0200

    Some fixes, still 2Test todo

--- parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
+++ parkingsystem/src/test/java/com/parkit/parkingsystem/ParkingServiceTest.java
@@ -1,21 +1,20 @@
 package com.parkit.parkingsystem;
 
-import com.parkit.parkingsystem.config.DataBaseConfig;
 import com.parkit.parkingsystem.constants.ParkingType;
 import com.parkit.parkingsystem.dao.ParkingSpotDAO;
 import com.parkit.parkingsystem.dao.TicketDAO;
-import com.parkit.parkingsystem.integration.config.DataBaseTestConfig;
 import com.parkit.parkingsystem.model.ParkingSpot;
 import com.parkit.parkingsystem.model.Ticket;
 import com.parkit.parkingsystem.service.ParkingService;
 import com.parkit.parkingsystem.util.InputReaderUtil;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
-import java.sql.Connection;
+
+
 
 import java.util.Date;
 
@@ -32,7 +31,7 @@ public class ParkingServiceTest {
     private static ParkingSpotDAO parkingSpot;
     @Mock
     private static TicketDAO ticketDAO;
-
+    private Ticket ticket;
     @BeforeEach
     public void setUp() {
       try {
@@ -42,44 +41,64 @@ public class ParkingServiceTest {
         ticket.setVehicleRegNumber("ABCDEF");
         ticket.setInTime(new Date(System.currentTimeMillis() - (60 * 60 * 1000)));
         ticket.setParkingSpot(parkingSpot);
-        when(inputReaderUtil.readVehicleRegistrationNumber()).thenReturn("ABCDEF");
-        
-        when(ticketDAO.getTicket(anyString())).thenReturn(ticket);
+        ticket.setPrice(0);
+        when(inputReaderUtil.readSelection()).thenReturn(1);
+        when(ticketDAO.saveTicket(any(Ticket.class))).thenReturn(true);
+        ticketDAO.saveTicket(ticket);
+
+
+
+
 
       } catch(Exception e) {
         throw new IllegalArgumentException("Entered input is invalid");
       }
     }
-  
+    @AfterEach
+    public void tearDown() {
+        parkingService = null;
+        inputReaderUtil = null;
+        parkingSpot = null;
+        ticketDAO = null;
+        ticket = null;
+    }
   @Test
   public void testProcessExitingVehicle() {
-      when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(false);
-      // When
-      parkingService.processExitingVehicle();
+    //Given
 
-      // Then
-      verify(ticketDAO, times(1)).updateTicket(any(Ticket.class));
-  }
-  
-   public void testProcessIncomingVehicle() {
-    // Given
     when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(true);
 
     // When
-    parkingService.processIncomingVehicle();
+    parkingService.processExitingVehicle();
 
     // Then
-    verify(ticketDAO, times(1)).updateTicket(any(Ticket.class));
+    verify(ticketDAO, times(1)).getNbTicket();
+  }
+  @Test
+   public void testProcessIncomingVehicle() throws Exception {
+
+    when(parkingSpot.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
+
+
+    parkingService.processIncomingVehicle();
+
+    verify(parkingSpot, times(1)).getNextAvailableSlot(any(ParkingType.class));
+
   }    
 
   @Test
   public void processExitingVehicleTestUnableUpdate() {
-    
 
+    when(ticketDAO.updateTicket(any(Ticket.class))).thenReturn(false);
+
+    parkingService.processExitingVehicle();
+
+    verify(ticketDAO, times(1)).updateTicket(any(Ticket.class));
   }
 
   @Test
   public void testGetNextParkingNumberIfAvailable() {
+
     when(parkingSpot.getNextAvailableSlot(any(ParkingType.class))).thenReturn(1);
 
     parkingService.getNextParkingNumberIfAvailable();
@@ -90,18 +109,20 @@ public class ParkingServiceTest {
 
   public void testGetNextParkingNumberIfAvailableParkingNumberNotFound()  {
 
-    
+    when(parkingSpot.getNextAvailableSlot(any(ParkingType.class))).thenReturn(-1);
 
     parkingService.getNextParkingNumberIfAvailable();
 
+    verify(parkingSpot, times(1)).getNextAvailableSlot(any(ParkingType.class));
 
   }
-  @Test 
+  @Test
   public void testGetNextParkingNumberIfAvailableParkingNumberWrongArgument() {
-    
-    
+    when(parkingSpot.getNextAvailableSlot(any(ParkingType.class))).thenReturn(0);
+
     parkingService.getNextParkingNumberIfAvailable();
 
+    verify(parkingSpot, times(1)).getNextAvailableSlot(any(ParkingType.class));
     
   } 
 
